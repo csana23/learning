88c656e36d5dffcd575c309e2bdc8d0ef7f4ff23
Richard Csanaki <richard.csanaki@gmail.com>
2024-05-29 14:47:17 +0200

    leetcode prob

--- general/185.py
+++ general/185.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 employee_data = {
     'id': [1, 2, 3, 4, 5, 6, 7],
@@ -13,5 +14,27 @@ department_data = {
 }
 
 def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    # make one table out of this
+    # get top 3 unique salaries by department
+    salaries = employee.groupby('departmentId')['salary'].unique().apply(lambda x: np.sort(x)[-3:])
+
+    df = pd.merge(left=employee, right=salaries, how="inner", on="departmentId")
+
+    df["is_top_3"] = df.apply(lambda row: "yes" if row["salary_x"] in row["salary_y"] else "no", axis=1)
+
+    result = pd.merge(left=df, right=department, how="inner", left_on="departmentId", right_on="id")
+
+    result = result.loc[result.is_top_3 == "yes"]
+
+    drop_cols = ["id_x", "departmentId", "salary_y", "is_top_3", "id_y"]
+    result = result.drop(drop_cols, axis=1)
+
+    result = result.rename(columns={"name_x": "Employee", "name_y": "Department", "salary_x": "Salary"})
+
+    return result
     
+
+employee_df = pd.DataFrame(employee_data)
+
+department_df = pd.DataFrame(department_data)
+
+print(top_three_salaries(employee_df, department_df))
