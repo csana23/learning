56cc7dc221cec6c333c305a34e78ad185e3d83c7
Richard Csanaki <richard.csanaki@gmail.com>
2024-06-12 21:47:31 +0200

    update

--- general/1.py
+++ general/1.py
@@ -3,7 +3,7 @@ class Solution:
         sum = 0
         step = 0
         for i in range(0, len(nums)):
-            for j in range(1, len(nums)):
+            for j in range(i+1, len(nums)):
                 print(f"step #{step}")
                 sum = nums[i] + nums[j]
                 step += 1
@@ -13,7 +13,7 @@ class Solution:
 
 s = Solution()
 
-nums = [3,3]
-target = 6
+nums = [11,15,2,7]
+target = 9
 
 print(s.twoSum(nums, target))
